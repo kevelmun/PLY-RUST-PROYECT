a9b308d0e8b2bcb846cceede296c8f9959f4ee06
DanielGR98 <daniguer@espol.edu.ec>
2022-11-20 17:31:28 -0500

    avance sintactico

--- pyacc.py
+++ pyacc.py
@@ -21,6 +21,37 @@ def p_expression(p):
     | void'''
 
 #DANIEL GUERRERO
+def p_data(p):
+    '''data : CADENA
+    | INTEGER
+    | CHAR
+    | FLOAT
+    | KW_FALSE
+    | KW_TRUE'''
+
+def p_comparator(p):
+    '''comparator :
+    | EQ
+    | NE
+    | GE
+    | LE
+    | GT
+    | LT'''
+
+def p_and(p):
+    'and : VARIABLE LAND VARIABLE'
+
+def p_conditional(p):
+    '''conditional : VARIABLE comparator data
+    | and comparator data'''
+
+def p_or(p):
+    'or : VARIABLE LOR VARIABLE'
+
+def p_mdata(p):
+    '''mdata : data
+    | data COMMA mdata'''
+
 def p_print(p):
     'print : KW_PRINT LPAREN CADENA RPAREN SEMI'
 
@@ -28,17 +59,21 @@ def p_declare_variable(p):
     'dvariable : KW_LET VARIABLE SEMI'
 
 def p_array(p):
-    'array : LBRACKET RBRACKET'
+    'array : KW_LET VARIABLE EQUALS LBRACKET mdata RBRACKET'
 
 def p_variables(p):
     '''variables : VARIABLE 
     | VARIABLE COMMA variables'''
 
+
+def p_while(p):
+    'while : KW_WHILE LPAREN VARIABLE '
+
 # KEVIN MUÑOZ
 
 # Function without arguments   
 def p_function(p):
-    'function : KW_FN VARIABLE LPAREN RPAREN LBRACKET expression RBRACKET'
+    'function : KW_FN VARIABLE LPAREN RPAREN LBRACE expression RBRACE'
 
 def p_function_argument(p):
     'function_argument : KW_FN VARIABLE LPAREN variables RPAREN LBRACKET expression RBRACKET'
